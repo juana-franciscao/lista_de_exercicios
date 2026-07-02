6f9f5bdc8f648a61ab1f758ac70f78230bbcc257
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-02 08:19:08 -0300

    add -  final do exercicio 1

---
+++ ex_1.php
@@ -0,0 +1,24 @@
+<?php
+
+//(x² + y²) / (x+y)
+
+function calcularFormula($x,$y)
+{
+
+    if(($x+$y)== 0){
+
+        return "Não é possível realizar a divisão por zero";
+
+    }
+
+    $resultado = ((pow($x,2)) + pow ($y,2))/($x+$y);
+    return $resultado;
+}
+
+    $x = 10;
+    $y = 5;
+
+    echo "Valor de x é:  $x <br>";
+    echo "Valor de y é:  $y <br>";
+
+    echo "<br> Resultado: " . calcularFormula($x,$y);
