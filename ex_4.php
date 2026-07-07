2f4866ad9437d36f598ab944d02e65c13ce9410b
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-07 07:39:13 -0300

    add- finalizei exercicio 4

---
+++ ex_4.php
@@ -0,0 +1,31 @@
+<?php
+
+function gerarSenha($tamanho, $maiusculas, $minusculas, $numeros, $especiais)
+{
+    $maiusculas = "ABCDEFGHIJKLMNOPQRSTUVYXWZ"; // letras maiúsculas
+    $minusculas = "abcdefghijklmnopqrstuvyxwz"; // letras minúsculas
+    $numeros = "0123456789"; // números
+    $especiais = "!@#$%¨&*()_+="; // símbolos
+
+    $senha = "";
+
+    if ($maiusculas) {
+        $senha .= str_shuffle($maiusculas);
+    }
+
+    if ($minusculas) {
+        $senha .= str_shuffle($minusculas);
+    }
+
+    if ($numeros) {
+        $senha .= str_shuffle($numeros);
+    }
+
+    if ($especiais) {
+        $senha .= str_shuffle($especiais);
+    }
+
+    return substr(str_shuffle($senha), 0, $tamanho);
+}
+
+ echo "Senha gerada: " . gerarSenha(10, true, true, true, true);
