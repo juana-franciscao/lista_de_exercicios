8274a35532294a7b3f6e2c1b2a06eed9db5bbd14
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-07 08:10:27 -0300

    add - iniciei e finalizei atividade 5

---
+++ ex_5.php
@@ -0,0 +1,17 @@
+<?php
+function  analisarTexto($texto)
+{
+   
+    $palavras = str_word_count($texto);
+    $caracteres = strlen($texto);
+    $frases = substr_count($texto, '.');
+    $vogais = preg_match_all('/[aeiou]/i', $texto);
+    $consoantes = preg_match_all('/[bcdfghjklmnpqrstvwxyz]/i', $texto);
+
+    return "O texto tem: $caracteres caracteres, $palavras palavras, $frases frases, $vogais vogais e $consoantes consoantes.";
+
+}
+
+    $texto = "Denise, sai da live filha.";
+    echo "Texto original: " . $texto . "<br>";
+    echo analisarTexto($texto);    
