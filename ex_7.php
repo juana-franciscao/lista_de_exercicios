7114144a7bb817900ab01a70703f28940bd0cff9
Juana-Franciscao-2213 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-07 09:23:19 -0300

    add - finalizei a atividade 7

--- ex_7.php
+++ ex_7.php
@@ -1,4 +1,31 @@
 <?php
 
 function calcularDesconto($valorCompra)
-{}
+{
+    
+    if($valorCompra > 1000){
+        $desconto = 30;
+    }
+    elseif($valorCompra > 500){
+        $desconto = 20;
+    }
+    elseif($valorCompra > 100){
+        $desconto = 10;
+    }
+    else{
+        $desconto = 0;
+    }
+
+    
+    $valorDesconto = ($valorCompra * $desconto) / 100;
+
+    $valorFinal = $valorCompra - $valorDesconto;
+
+    echo "Valor original: R$ " . ($valorCompra) . "<br>";
+    echo "Desconto aplicado: " . $desconto . "%<br>";
+    echo "Valor do desconto: R$ " . ($valorDesconto) . "<br>";
+    echo "Valor final: R$ " .($valorFinal) . "<br>";
+}
+
+
+calcularDesconto(1804.50);
