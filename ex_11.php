aab27a4d47143ee578bfe97cb7ce460240690c90
juana-franciscao-2215 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-08 10:13:28 -0300

    add - iniciei e finalizei  a atividade 11

--- ex_11.php
+++ ex_11.php
@@ -1,5 +1,20 @@
 <?php  
 
- function formatarTexto(){
-    
+ function formatarTexto( string $texto): array {
+
+    return[
+        'maiusculos' => mb_strtoupper($texto, 'UTF-8'),
+        'minusculos' => mb_strtolower($texto, 'UTF-8'),
+        'primeiraLetraMaiuscula' => mb_convert_case($texto, MB_CASE_TITLE, 'UTF-8'),
+        'quantidadeCaracteres' => mb_strlen($texto, 'UTF-8')
+    ]
+
  }
+
+ $textoOriginal = "Denise, sai da live filha";
+ $resultados = formatarTexto($textoOriginal);
+
+ echo "Texto em maiúsculas: " . $resultados['maiusculos'] . "<br>";
+ echo "Texto em minúsculas: " . $resultados['minusculos'] . "<br>";    
+ echo "Texto com primeira letra maiúscula: " . $resultados['primeiraLetraMaiuscula'] . "<br>";
+ encho "Quantidade de caracteres: " . $resultados['quantidadeCaracteres'] . "<br>";
