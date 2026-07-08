e25353910a14ee4109e8b5ab1ca3733701915e65
Juana-Casa <juana_franciscao@estudante.sesisenai.org.br>
2026-07-08 20:08:14 -0300

    up - terminei a atividade 13

--- ex_13.php
+++ ex_13.php
@@ -2,4 +2,46 @@
 
 function criptografarMensagem($texto){
 
-   }
+    $resultado = "";
+
+    for($i = 0; $i < strlen($texto); $i++){
+
+        $letra = $texto[$i];
+
+        if($letra != " "){
+            $resultado .= chr(ord($letra) + 3);
+        }else{
+            $resultado .= " ";
+        }
+    }
+
+    return $resultado;
+}
+
+function descriptografarMensagem($texto){
+
+    $resultado = "";
+
+    for($i = 0; $i < strlen($texto); $i++){
+
+        $letra = $texto[$i];
+
+        if($letra != " "){
+            $resultado .= chr(ord($letra) - 3);
+        }else{
+            $resultado .= " ";
+        }
+    }
+
+    return $resultado;
+}
+
+$mensagem = "Denise, sai da live filha";
+
+$criptografada = criptografarMensagem($mensagem);
+
+echo "Mensagem original: " . $mensagem . "<br>";
+echo "Mensagem criptografada: " . $criptografada . "<br>";
+echo "Mensagem descriptografada: " . descriptografarMensagem($criptografada);
+
+?>
