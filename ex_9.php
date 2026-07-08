c1058d43c4edfe28db458729692388efe198fcae
juana-franciscao-2215 <juana_franciscao@estudante.sesisenai.org.br>
2026-07-08 09:17:49 -0300

    add-  iniciei e finalizei a atividade 9

---
+++ ex_9.php
@@ -0,0 +1,39 @@
+<?php
+
+function analisarNumero($numero) {
+ 
+//numero par ou ímpar
+$parOuImpar = ($numero % 2 == 0) ? "par" : "ímpar";
+//final numero par ou ímpar
+
+
+//numero primo
+$primo= true;
+if ($numero <= 1) {
+    $primo = false;
+} else {
+    for ($i = 2; $i <= sqrt($numero); $i++) {
+        if ($numero % $i == 0) {
+            $primo = false;
+            break;
+        }
+    }
+}
+    $numeroPrimo = $primo ? "é primo" : "não é primo";
+//final numero primo
+
+
+//numero perfeito
+$somaDivisores = 0;
+for ($i = 1; $i < $numero; $i++) {
+    if ($numero % $i == 0) {
+        $somaDivisores += $i;
+    }
+}
+$numeroPerfeito = ($somaDivisores == $numero) ? "é perfeito" : "não é perfeito";
+//final numero perfeito
+
+
+}
+
+ return [$parOuImpar, $numeroPrimo, $numeroPerfeito];
