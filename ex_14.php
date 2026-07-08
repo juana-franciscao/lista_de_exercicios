19a0a00be1ab1639b9c549c72ec084afe5873d11
Juana-Casa <juana_franciscao@estudante.sesisenai.org.br>
2026-07-08 20:23:35 -0300

    up - finalizei a questão 14

--- ex_14.php
+++ ex_14.php
@@ -2,4 +2,56 @@
 
 function estatisticasNumericas($numeros){
 
+}$soma = 0;
+    $pares = 0;
+    $impares = 0;
+
+    $maior = $numeros[0];
+    $menor = $numeros[0];
+
+    foreach($numeros as $numero){
+
+        $soma += $numero;
+
+        if($numero > $maior){
+            $maior = $numero;
+        }
+
+        if($numero < $menor){
+            $menor = $numero;
+        }
+
+        if($numero % 2 == 0){
+            $pares++;
+        }else{
+            $impares++;
+        }
+    }
+
+    $media = $soma / count($numeros);
+
+    // Ordena o vetor para calcular a mediana
+    sort($numeros);
+
+    $quantidade = count($numeros);
+
+    if($quantidade % 2 == 0){
+        $mediana = ($numeros[$quantidade / 2 - 1] + $numeros[$quantidade / 2]) / 2;
+    }else{
+        $mediana = $numeros[floor($quantidade / 2)];
+    }
+
+    return "Soma: $soma<br>" .
+           "Média: $media<br>" .
+           "Maior valor: $maior<br>" .
+           "Menor valor: $menor<br>" .
+           "Mediana: $mediana<br>" .
+           "Quantidade de números pares: $pares<br>" .
+           "Quantidade de números ímpares: $impares";
 }
+
+// Vetor de números
+$numeros = [1, 6, 7, 67, 18, 4, 21];
+
+
+echo estatisticasNumericas($numeros);
