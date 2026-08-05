2066fafdd8b6a0d3847b6a181a4c1c60d9183ac1
juana-franciscao-2215 <juana_franciscao@estudante.sesisenai.org.br>
2026-08-05 09:16:04 -0300

    up - atualizei e corrigi a atividade 16

--- exercicio_complementar_16.php
+++ exercicio_complementar_16.php
@@ -1,19 +1,61 @@
 <?php
 
-function  analisarSenha()($senha) {
-    $temMaiuscula = preg_match('/[A-Z]/', $senha);
-    $temMinuscula = preg_match('/[a-z]/', $senha);
-    $temNumero = preg_match('/[0-9]/', $senha);
-    $temCaracterEspecial = preg_match('/[\W_]/', $senha);
+function  analisarSenha($senha) {
+
+    $qtdMaiusculas = contarMaiusculas($senha);    
+    $qtdMinusculas = contarMinusculas($senha);
+    $qtdNumeros = contarNumeros($senha);
+    $qtdCaracteresEspeciais = contarCaracteresEspeciais($senha);
     $tamanho = strlen($senha);
 
-    
-    if ($tamanho < 8) {
-        return "Senha fraca: deve ter pelo menos 8 caracteres.";
-    } elseif (!$temMaiuscula || !$temMinuscula || !$temNumero || !$temCaracterEspecial) {
-        return "Senha média: deve conter letras maiúsculas, minúsculas, números e caracteres especiais.";
-    } else {
-        return "Senha forte.";
+    $nivelSeguranca = validarSenha($senha);
+
+    return [
+        'senha' => $senha,
+        'qtdMaiusculas' => $qtdMaiusculas,
+        'qtdMinusculas' => $qtdMinusculas,
+        'qtdNumeros' => $qtdNumeros,
+        'qtdCaracteresEspeciais' => $qtdCaracteresEspeciais,
+        'tamanho' => $tamanho,
+        'nivelSeguranca' => $nivelSeguranca
+    ];
+
+}
+
+function Maiusculas($senha) {
+    return preg_match('/[A-Z]/', $senha);
+
+}
+
+function Minusculas($senha) {
+    return preg_match('/[a-z]/', $senha);
+}
+
+function Numeros($senha) {
+    return preg_match_all('/[0-9]/', $senha, $matches) >= 2;
+}
+
+function CaracteresEspeciais($senha) {
+    return preg_match('/[\W_]/', $senha);
+}
+
+function tamanhoSenha($senha) {
+    return strlen($senha) >= 8;
+}
+
+function validarSenha($senha) {
+
+    if(tamanhoSenha($senha)  && Maiusculas($senha) && Minusculas($senha) && Numeros($senha) && CaracteresEspeciais($senha)){
+        return "Senha forte Muito";
+    }  
+    if(tamanhoSenha($senha) && Maiusculas($senha) && Minusculas($senha) && quantidadeNumeros($senha)){
+        return "Senha forte";
+    }
+     if(tamanhoSenha($senha) && Maiusculas($senha)&& Minusculas($senha)){
+        return "Senha media";
+    }
+    if(tamanhoSenha($senha) && Maiusculas($senha) ){
+        return "Senha fraca";
     }
 }
 
